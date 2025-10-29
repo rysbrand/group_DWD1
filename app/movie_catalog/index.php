72c25217fa6ad157b0bd4de4570cb4df9daacb77
dakaleeb <deltasquadgamer@gmail.com>
2025-10-28 22:06:18 -0500

    cart stuff and other stuff

--- app/movie_catalog/index.php
+++ app/movie_catalog/index.php
@@ -8,9 +8,29 @@ require('../models/movies_db.php');
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
-    if ($action == NULL) {
-        $action = 'list_movies';
+}
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'r'); // <-- Add this line
+}
+
+if ($action == NULL) {
+    $action = 'list_movies';
+}
+
+if ($action == 'list_movies') {
+    $movies = get_movies();
+    include('../movie_catalog/movie_view.php');
+}
+elseif ($action == 'view_movie') {
+    $movie_id = filter_input(INPUT_GET, 'movie_id', FILTER_VALIDATE_INT);
+    if ($movie_id) {
+        $movie = get_movie($movie_id);
+        $movies = get_movies(); 
+        include('../movie_catalog/movie_view.php');
+    } else {
+        echo "<p>Invalid movie ID.</p>";
     }
 }
+
 
 ?>
